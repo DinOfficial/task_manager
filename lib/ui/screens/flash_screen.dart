0faf92268dc333c239c5fea8fcb1e09842efda1c
DinOfficial <shdiinofficial19@gmail.com>
2025-11-06 11:30:16 +0300

    sing in page create

--- lib/ui/screens/flash_screen.dart
+++ lib/ui/screens/flash_screen.dart
@@ -2,16 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:untitled/ui/screens/log_in_screen.dart';
 import 'package:untitled/ui/utils/asset_path.dart';
+import 'package:untitled/ui/widgets/screen_background.dart';
 
 class FlashScreen extends StatefulWidget {
   const FlashScreen({super.key});
+
+  final String name = '/flash-screen';
 
   @override
   State<FlashScreen> createState() => _FlashScreenState();
 }
 
 class _FlashScreenState extends State<FlashScreen> {
-
   @override
   void initState() {
     super.initState();
@@ -20,20 +22,13 @@ class _FlashScreenState extends State<FlashScreen> {
 
   Future<void> _navigate() async {
     await Future.delayed(const Duration(seconds: 2));
-    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>LogInScreen()));
+    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LogInScreen()));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          SvgPicture.asset(AssetPath.backgroundSVG, fit: BoxFit.cover,width: double.maxFinite,height: double.maxFinite,),
-          Center(
-              child: SvgPicture.asset(AssetPath.logoSVG, width: 120, )
-          ) ,
-        ],
-      ),
+      body: ScreenBackground(child: Center(child: SvgPicture.asset(AssetPath.logoSVG, width: 120))),
     );
   }
 }
--- lib/ui/screens/log_in_screen.dart
+++ lib/ui/screens/log_in_screen.dart
@@ -1,7 +1,10 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:untitled/ui/widgets/screen_background.dart';
 
 class LogInScreen extends StatefulWidget {
   const LogInScreen({super.key});
+  final String name = '/log-in-screen';
 
   @override
   State<LogInScreen> createState() => _LogInScreenState();
@@ -11,9 +14,52 @@ class _LogInScreenState extends State<LogInScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Log In Screen'),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+        child: ScreenBackground(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            spacing: 15,
+            children: [
+              const SizedBox(height: 100),
+              Text('Get Started With', style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600)),
+              TextField(decoration: InputDecoration(hintText: 'Email')),
+              TextField(decoration: InputDecoration(hintText: 'Password')),
+              FilledButton(
+                onPressed: () {},
+                style: FilledButton.styleFrom(),
+                child: Icon(Icons.arrow_forward_ios_outlined),
+              ),
+              const SizedBox(height: 40),
+              Center(
+                child: Column(
+                  children: [
+                    TextButton(onPressed: (){}, child: Text('Forgot Password?', style: TextStyle(fontSize: 18),)),
+                    RichText(text: TextSpan(
+                        text: 'Don\'t have an account? ',
+                        style: TextStyle(color: Colors.grey, fontSize: 18),
+                        children: [
+                          TextSpan(text: 'Sign Up', style: TextStyle(color: Colors.green), recognizer: TapGestureRecognizer()..onTap = () {} )
+                        ]
+                    ),
+                    )
+                  ],
+                ),
+              )
+
+            ],
+          ),
+        ),
       ),
     );
   }
+
+
+  void _onNavigate(){}
+
+  void _onForgotPassword(){}
+
+  void _onSignUp(){}
+
 }
