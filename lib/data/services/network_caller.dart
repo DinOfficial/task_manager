2d81009300ffe6cff50c0dbcaa8b17d09e5c29d5
DinOfficial <shdinofficial19@gmail.com>
2025-11-15 00:04:33 +0300

    model and services add

---
+++ lib/data/services/network_caller.dart
@@ -0,0 +1,93 @@
+import 'dart:convert';
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+
+class NetWorkCaller {
+  Future<NetworkResponse> getRequest(String url) async {
+    try {
+      Uri uri = Uri.parse(url);
+      _logRequest(url);
+      final response = await http.get(uri);
+      _logResponse(url, response);
+      final decodedResponse = jsonDecode(response.body);
+
+      if (response.statusCode == 200) {
+        return NetworkResponse(
+          isSuccess: true,
+          responseCode: response.statusCode,
+        );
+      } else {
+        return NetworkResponse(
+          isSuccess: false,
+          responseCode: response.statusCode,
+          errorMessage: decodedResponse['body'],
+        );
+      }
+    } catch (e) {
+      return NetworkResponse(
+        isSuccess: false,
+        responseCode: -1,
+        errorMessage: e.toString(),
+      );
+    }
+  }
+
+  Future<NetworkResponse> postRequest(String url,
+      Map<String, dynamic>? body) async {
+    try {
+      Uri uri = Uri.parse(url);
+      _logRequest(url);
+      final response = await http.post(
+        uri,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: body,
+      );
+      _logResponse(url, response);
+      final decodedResponse = jsonEncode(response.body);
+
+      if (response.statusCode == 200) {
+        return NetworkResponse(
+          isSuccess: true,
+          responseCode: response.statusCode,
+          body: decodedResponse,
+        );
+      } else {
+        return NetworkResponse(
+          isSuccess: false,
+          responseCode: response.statusCode,
+        );
+      }
+    } catch (e) {
+      return NetworkResponse(
+        isSuccess: false,
+        responseCode: -1,
+        errorMessage: e.toString(),
+      );
+    }
+  }
+  void _logRequest(String url){
+    debugPrint('Request: $url');
+  }
+
+  void _logResponse(String url, dynamic response){
+    debugPrint('Url: $url\n'
+        'Response: ${response.statusCode}\n'
+        'Body: ${response.body}');
+  }
+}
+
+class NetworkResponse {
+  final bool isSuccess;
+  final int responseCode;
+  final dynamic body;
+  final String? errorMessage;
+
+  NetworkResponse({
+    required this.isSuccess,
+    required this.responseCode,
+    this.body,
+    this.errorMessage,
+  });
+}
